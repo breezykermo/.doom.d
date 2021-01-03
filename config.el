fe43cefe418f565d6c1aef1b5ce5afe1317919bb
Lachlan <Kermode>
2021-01-03 21:09:33 +0100

    transparent, nicer font

--- config.el
+++ config.el
@@ -15,20 +15,19 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "sourcecodepro" :size 24)
+       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(set-frame-parameter (selected-frame) 'alpha '(85 50))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
-(setq doom-font (font-spec :family "JuliaMono" :size 18))
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Dropbox/obsidian/org")
-
 
 (after! org
         (setq org-log-done 'time) ;; add timestamps to DONE
